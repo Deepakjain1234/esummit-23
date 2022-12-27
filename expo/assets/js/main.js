0ff7ca4bfd90db27782da68cef02d6e50f14600e
deepak <deeepakjain132@gmail.com>
2022-12-27 23:56:34 +0530

    change

---
+++ expo/assets/js/main.js
@@ -0,0 +1,100 @@
+var config = {
+    apiKey: "AIzaSyDfb2QT1AG3-2yqiiUo1mkOn170QRtK92A",
+    authDomain: "ecell-1b04d.firebaseapp.com",
+    projectId: "ecell-1b04d",
+    storageBucket: "ecell-1b04d.appspot.com",
+    messagingSenderId: "324153281020",
+    appId: "1:324153281020:web:0d3d83bdb18d6e0f534a79",
+    measurementId: "G-BW02JFHNT6",
+    databaseURL: "https://ecell-1b04d-default-rtdb.firebaseio.com/"
+};
+firebase.initializeApp(config);
+
+//Reference for form collection(3)
+let formMessage = firebase.database().ref('StartupRegistration');
+
+//listen for submit event//(1)
+document
+    .getElementById('registration')
+    .addEventListener('submit', formSubmit);
+
+
+//Submit form(1.2)
+function formSubmit(e) {
+    console.log("submit",e);
+    e.preventDefault();
+    // Get Values from the DOM
+    let data =
+    {
+        name: document.querySelector('#name').value,
+        email: document.querySelector('#email').value,
+        branch: document.querySelector('#branch').value,
+        year: document.querySelector('#year').value,
+        college: document.querySelector('#college').value,
+        contact: document.querySelector('#phone').value,
+        refferal: document.querySelector('#referral').value,
+    }
+
+
+
+
+
+
+
+    //send message values
+    sendMessage(data);
+
+    //Show Alert Message(5)
+    document.querySelector('.alert').style.display = 'block';
+
+    //Hide Alert Message After Seven Seconds(6)
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 7000);
+
+    //Form Reset After Submission(7)
+    document.getElementById('registrationform').reset();
+}
+
+
+//Send Message to Firebase(4)
+
+function sendMessage(data) {
+    let newFormMessage = formMessage.push();
+
+    // alert("submit")
+    newFormMessage.set(data);
+    // alert("your application submited");
+
+    swal({title: "Good job", text: "Registered successfully!", type: 
+"success"}).then(function(){ 
+   location.reload();
+   }
+);
+    // location.reload();
+    // displayMessage();
+
+
+
+    // // data to be sent to the POST request
+    // let _data = {
+    //     email: email,
+    //     name: name,
+    //     vertical: vertical1,
+    //     vertical2: vertical2,
+    //     contact: contact
+
+    // }
+
+    // fetch('20.24.196.91:8080', {
+    //     method: "POST",
+    //     body: JSON.stringify(_data),
+    //     headers: { "Content-type": "application/json; charset=UTF-8" }
+    // })
+    //     .then(response => response.text())
+    //     .then(json => { console.log(json); window.location.href = "index.html"; });
+
+
+
+
+}
