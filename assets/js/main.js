9e7b33c4fee2da4971e9c382a68adf28bca463ec
Shrish Shrivastava <shrish108@gmail.com>
2023-02-03 19:42:37 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+
+var config = {
+    apiKey: "AIzaSyDfb2QT1AG3-2yqiiUo1mkOn170QRtK92A",
+    authDomain: "ecell-1b04d.firebaseapp.com",
+    projectId: "ecell-1b04d",
+    storageBucket: "ecell-1b04d.appspot.com",
+    messagingSenderId: "324153281020",
+    appId: "1:324153281020:web:0d3d83bdb18d6e0f534a79",
+    measurementId: "G-BW02JFHNT6",
+    databaseURL: "https://ecell-1b04d-default-rtdb.firebaseio.com/"
+};
+firebase.initializeApp(config);
+
+//Reference for form collection(3)
+let formMessage = firebase.database().ref('pass');
+
+//listen for submit event//(1)
+
+
+//Submit form(1.2)
+function formSubmit(e) {
+    // alert("eor")
+    console.log("submit",e);
+    e.preventDefault();
+    document.getElementById('college').removeAttribute('required')
+    // Get Values from the DOM
+    let data =
+{
+   name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    contact: document.querySelector('#contact').value,
+    year: document.querySelector('#year').value,
+    branch: document.querySelector('#branch').value, 
+    college: "manit", 
+    code: document.querySelector('#code').value
+   
+}
+    console.log(data);
+
+
+
+
+
+
+
+    //send message values
+    sendMessage(data);
+
+    //Show Alert Message(5)
+    document.querySelector('.alert').style.display = 'block';
+
+    //Hide Alert Message After Seven Seconds(6)
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 7000);
+
+    //Form Reset After Submission(7)
+    document.getElementById('registrationform').reset();
+}
+
+
+//Send Message to Firebase(4)
+
+function sendMessage(data) {
+    let newFormMessage = formMessage.push();
+
+    // alert("submit")
+    newFormMessage.set(data);
+    // alert("your application submited");
+
+    swal({title: "Good job", text: "Registered successfully!", type: 
+"success"}).then(function(){ 
+   location.reload();
+   }
+);
+    // location.reload();
+    // displayMessage();
+
+
+
+    // // data to be sent to the POST request
+    // let _data = {
+    //     email: email,
+    //     name: name,
+    //     vertical: vertical1,
+    //     vertical2: vertical2,
+    //     contact: contact
+
+    // }
+
+    // fetch('20.24.196.91:8080', {
+    //     method: "POST",
+    //     body: JSON.stringify(_data),
+    //     headers: { "Content-type": "application/json; charset=UTF-8" }
+    // })
+    //     .then(response => response.text())
+    //     .then(json => { console.log(json); window.location.href = "index.html"; });
+
+
+
+
+}
+
